e90117c408e4d4b5b6b61c0e63040b573803a315
Pascallo <110603467+miro187@users.noreply.github.com>
2025-07-05 21:57:19 +0200

    Update SECRETS.js

--- SECRETS.js
+++ SECRETS.js
@@ -1,11 +1,11 @@
 //? It is advisable to use environment variables instead of directly putting secrets in repository file but I have skipped this part as it would become complicated for many.
 //? Alternatively you can download the repository instead or forking and upload it from your account and keep it private, in that way, your secrets will not be exposed to the public.
 
-const APP_KEY = "YOUR_TWITTER_APP_KEY";
-const APP_SECRET = "YOUR_TWITTER_APP_SECRET";
-const ACCESS_TOKEN = "YOUR_TWITTER_ACCESS_TOKEN";
-const ACCESS_SECRET = "YOUR_TWITTER_ACCESS_SECRET";
-const GEMINI_API_KEY = "YOUR_GEMINI_API_KEY";
+const APP_KEY = "jgyGmjTQiFUYzPblfnHatJeVf";
+const APP_SECRET = "SoqYVRjFt74t58sEyuGMKSfrci4MZBEZzMEYSxZCz3okNqgRpp";
+const ACCESS_TOKEN = "1392947428333170689-Qj9BBnvGQdoibHYMgabPCh1iUY1iI1";
+const ACCESS_SECRET = "JAPVTEImQqa1dvVnzSwko1QcFaJKM6mADCEkBs2oL3nAA";
+const GEMINI_API_KEY = "AIzaSyBMsxXiYoZ6UzJmLUwoWwjQq8zQnfTX4rg";
 
 const SECRETS = {
   APP_KEY,
